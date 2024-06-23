04a51afec6847299fe3e1b7647c6172920edac79
John Westbrook <156149716+chantikoiii@users.noreply.github.com>
2024-06-23 13:50:40 -0400

    Update Pt4.py

--- Module 6/Pt4.py
+++ Module 6/Pt4.py
@@ -2,10 +2,11 @@ from datetime import datetime
 
 def treeCheck(f,i):
     while i >= 12:
+# assuring inch input does not exceed 1 foot
         print('Inches must not exceed 11. Please reenter:')
         n=int(input())
         i=n
-    
+# incorporating current time & formating apperance
     setup = datetime.now()
     layout = setup.strftime('%A %b %e %Y')
     print('Since', layout, 'your tree is',f, '\'',i,'" tall!')
